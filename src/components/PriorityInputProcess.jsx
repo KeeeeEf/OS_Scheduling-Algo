8f3cff1e11015ca7789898d23e41615f159545f5
ysa <128650565+maelliecia@users.noreply.github.com>
2023-11-11 23:57:26 +0800

    Added components for PS and adjusted scheduling table

---
+++ src/components/PriorityInputProcess.jsx
@@ -0,0 +1,237 @@
+import React, { useState, useEffect } from 'react';
+import '/src/App.css';
+import { useNavigate } from 'react-router-dom';
+
+export const InputProcess = () => {
+  const navigate = useNavigate();
+  const [processes, setProcesses] = useState([]);
+  const [arrivalTime, setArrivalTime] = useState('');
+  const [cpuBurst, setCpuBurst] = useState('');
+  const [priority, setPriority] = useState('');
+  const [editId, setEditId] = useState(null);
+
+  const getNextProcessId = () => {
+    const lastProcess = processes[processes.length - 1];
+    if (!lastProcess) {
+      return 'A';
+    }
+    const lastId = lastProcess.id;
+    const nextCharCode = lastId.charCodeAt(0) + 1;
+    return String.fromCharCode(nextCharCode);
+  };
+
+  // add or edit process
+  const handleAddProcess = () => {
+    if (arrivalTime === '' || arrivalTime < 0) {
+      alert('Please enter valid arrival time');
+      return;
+    }
+
+    if (cpuBurst === '' || cpuBurst < 1) {
+      alert('Please enter valid CPU burst time.');
+      return;
+    }
+
+    if (priority === '' || priority < 1) {
+      alert('Please enter valid priority.');
+      return;
+    }
+
+    const remaining = parseInt(cpuBurst);
+
+    if (editId !== null) {
+      const updatedProcesses = processes.map((process) => {
+        if (process.id === editId) {
+          return {
+            id: process.id,
+            arrivalTime: parseInt(arrivalTime),
+            cpuBurst: parseInt(cpuBurst),
+            priority: parseInt(priority),
+            remainingTime: remaining,
+          };
+        }
+        return process;
+      });
+      setProcesses(updatedProcesses);
+      setEditId(null);
+    } else {
+      const newProcess = {
+        id: getNextProcessId(),
+        arrivalTime: parseInt(arrivalTime),
+        cpuBurst: parseInt(cpuBurst),
+        priority: parseInt(priority),
+        remainingTime: remaining,
+      };
+
+      setProcesses([...processes, newProcess]);
+    }
+
+    setArrivalTime('');
+    setCpuBurst('');
+    setPriority('');
+  };
+
+  // edit process
+  const handleEditProcess = (id) => {
+    const processToEdit = processes.find((process) => process.id === id);
+    if (processToEdit) {
+      setArrivalTime(processToEdit.arrivalTime.toString());
+      setCpuBurst(processToEdit.cpuBurst.toString());
+      setPriority(processToEdit.priority.toString());
+      setEditId(id);
+    }
+  };
+
+  // delete process
+  const handleDeleteProcess = (id) => {
+    setProcesses((prevProcesses) => prevProcesses.filter((process) => process.id !== id))
+    setEditId(null)
+
+    setProcesses((prevProcesses) => {
+      return prevProcesses.map((process, index) => ({
+        ...process,
+        id: String.fromCharCode('A'.charCodeAt(0) + index),
+      }))
+    })
+  }
+
+  useEffect(() => {
+    const storedProcesses = JSON.parse(sessionStorage.getItem('inputProcesses'))
+    if (storedProcesses) {
+      setProcesses(storedProcesses)
+    }
+  }, [])
+
+  const storeProcessesData = (data) => {
+    sessionStorage.setItem('inputProcesses', JSON.stringify(data))
+  }
+
+  const handleSimulate = () => {
+    storeProcessesData(processes)
+    navigate('/scheduling', {
+      state: {
+        processes,
+      },
+    })
+  }
+
+  return (
+    <div>
+      <h1>Input Processes</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Process ID</th>
+            <th>Arrival Time</th>
+            <th>CPU Burst</th>
+            <th>Priority</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {processes.map((process) => (
+            <tr key={process.id}>
+              <td>{process.id}</td>
+              <td>
+                {editId === process.id ? (
+                  <input
+                    type="number"
+                    value={arrivalTime}
+                    onChange={(e) => setArrivalTime(e.target.value)}
+                    className="form-control"
+                  />
+                ) : (
+                  process.arrivalTime
+                )}
+              </td>
+              <td>
+                {editId === process.id ? (
+                  <input
+                    type="number"
+                    value={cpuBurst}
+                    onChange={(e) => setCpuBurst(e.target.value)}
+                    className="form-control"
+                  />
+                ) : (
+                  process.cpuBurst
+                )}
+              </td>
+              <td>
+                {editId === process.id ? (
+                    <input
+                    type="number"
+                    value={priority}
+                    onChange={(e) => setPriority(e.target.value)}
+                    className="form-control"
+                    />
+                ) : (
+                    process.priority
+                )}
+              </td>
+              <td>
+                {editId === process.id ? (
+                  <button onClick={handleAddProcess} className="btn btn-primary">
+                    Save
+                  </button>
+                ) : (
+                  <>
+                    <button
+                      onClick={() => handleEditProcess(process.id)}
+                      className="btn btn-warning"
+                    >
+                      Edit
+                    </button>
+                    <button
+                      onClick={() => handleDeleteProcess(process.id)}
+                      className="btn btn-danger"
+                    >
+                      Delete
+                    </button>
+                  </>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <div className="form-group">
+        <label>Arrival Time:</label>
+        <input
+          type="number"
+          value={arrivalTime}
+          onChange={(e) => setArrivalTime(e.target.value)}
+          className="form-control"
+        />
+      </div>
+
+      <div className="form-group">
+        <label>CPU Burst:</label>
+        <input
+          type="number"
+          value={cpuBurst}
+          onChange={(e) => setCpuBurst(e.target.value)}
+          className="form-control"
+        />
+      </div>
+
+      <div className="form-group">
+        <label>Priority:</label>
+        <input
+          type="number"
+          value={priority}
+          onChange={(e) => setPriority(e.target.value)}
+          className="form-control"
+        />
+      </div>
+
+      <button onClick={handleAddProcess} className="btn btn-primary">
+        {editId !== null ? 'Save' : 'Add Process'}
+      </button>
+
+      <button onClick={handleSimulate} className="btn btn-primary">
+        Simulate
+      </button>
+    </div>
+  )
+}
--- src/components/PriorityScheduling.jsx
+++ src/components/PriorityScheduling.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import { calculateScheduling} from './algos/srtf';
+import { calculateScheduling} from './algos/pnp';
 import { GanttChart } from './GanttChart';
 
 export const Scheduling = () => {
@@ -14,6 +14,7 @@ export const Scheduling = () => {
     const originalData = functionCall.schedulingData;
     const sortedData = [...originalData].sort((a, b) => a.process.id.localeCompare(b.process.id));
     const timeline = functionCall.timelineList;
+  
     setSchedulingData(sortedData);
     setTimelineData(timeline);
   }, [processes]);
@@ -28,6 +29,7 @@ export const Scheduling = () => {
             <th>Process ID</th>
             <th>Arrival Time</th>
             <th>CPU Burst</th>
+            <th>Priority</th>
             <th>End Time</th>
             <th>Turnaround Time</th>
             <th>Waiting Time</th>
@@ -39,6 +41,7 @@ export const Scheduling = () => {
               <td>{data.process.id}</td>
               <td>{data.process.arrivalTime}</td>
               <td>{data.process.cpuBurst}</td>
+              <td>{data.process.priority}</td>
               <td>{data.endTime}</td>
               <td>{data.turnaroundTime}</td>
               <td>{data.waitingTime}</td>
